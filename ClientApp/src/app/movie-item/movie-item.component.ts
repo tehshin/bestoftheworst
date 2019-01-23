bc8c05da3133eb424fa932dc200b5eaf7d4c24a9
tehshin <thepikse@gmail.com>
2019-01-23 20:47:16 +0100

    movie by episode list styling

--- ClientApp/src/app/movie-item/movie-item.component.ts
+++ ClientApp/src/app/movie-item/movie-item.component.ts
@@ -2,28 +2,22 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Movie } from '../models/movie';
 
 @Component({
-  selector: 'app-movie-item',
-  templateUrl: './movie-item.component.html',
-  styleUrls: ['./movie-item.component.scss']
+    selector: 'app-movie-item',
+    templateUrl: './movie-item.component.html',
+    styleUrls: ['./movie-item.component.scss']
 })
-export class MovieItemComponent implements OnInit {
+export class MovieItemComponent {
 
-  @Input() movie: Movie;
+    @Input() movie: Movie;
 
-  constructor() { }
+    /**
+     * Returns an movies image for the requested width.
+     * @param movie Movie object
+     * @param width Width of the image
+     */
+    getImageUrlForWidth(movie: Movie, width: number): string {
+        if (!movie.image) return '';
 
-  ngOnInit(): void {
-  }
-
-  /**
-   * Returns an movies image for the requested width.
-   * @param movie Movie object
-   * @param width Width of the image
-   */
-  getImageUrlForWidth(movie: Movie, width: number): string {
-    if (!movie.image) return '';
-    
-    return movie.image.imageUrls[width] || movie.image.imageUrls[500];
-  }
-
+        return movie.image.imageUrls[width] || movie.image.imageUrls[500];
+    }
 }
