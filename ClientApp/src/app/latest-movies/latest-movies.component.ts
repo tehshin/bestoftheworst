f1b8d83cb96ba895ee8d40c3311016edf550afc9
tehshin <thepikse@gmail.com>
2019-02-07 22:15:13 +0100

    fix formatting and organize imports

--- ClientApp/src/app/latest-movies/latest-movies.component.ts
+++ ClientApp/src/app/latest-movies/latest-movies.component.ts
@@ -1,23 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-import { MovieService } from '../services/movie.service';
 import { Observable } from 'rxjs';
 import { EpisodeGroup } from '../models/episode-group';
+import { MovieService } from '../services/movie.service';
 
 @Component({
-  selector: 'app-latest-movies',
-  templateUrl: './latest-movies.component.html',
-  styleUrls: ['./latest-movies.component.scss']
+    selector: 'app-latest-movies',
+    templateUrl: './latest-movies.component.html',
+    styleUrls: ['./latest-movies.component.scss']
 })
 export class LatestMoviesComponent implements OnInit {
 
-  episodeGroups$: Observable<EpisodeGroup[]>;
+    episodeGroups$: Observable<EpisodeGroup[]>;
 
-  constructor(
-    private movieService: MovieService
-  ) { }
+    constructor(
+        private movieService: MovieService
+    ) { }
 
-  ngOnInit(): void {
-    this.episodeGroups$ = this.movieService.listLatestMoviesGroupedByEpisode();
-  }
+    ngOnInit(): void {
+        this.episodeGroups$ = this.movieService.listLatestMoviesGroupedByEpisode();
+    }
 
 }
